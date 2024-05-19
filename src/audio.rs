5099a50d49bb6aa41c7c45b7e0b14ef5760ccf63
xd009642 <danielmckenna93@gmail.com>
2024-05-19 18:31:24 +0100

    Add in decoding

--- src/audio.rs
+++ src/audio.rs
@@ -7,9 +7,25 @@ pub async fn decode_audio(
     mut rx: mpsc::Receiver<Bytes>,
     channel_data_tx: Vec<mpsc::Sender<AudioChannel>>,
 ) -> anyhow::Result<()> {
+    if channel_data_tx.is_empty() {
+        anyhow::bail!("No output sinks for channel data");
+    }
     while let Some(data) = rx.recv().await {
         if data.len() % 4 != 0 {
             anyhow::bail!("Got partial sample: {} bytes", data.len());
+        }
+        let mut channels = vec![
+            Vec::with_capacity(data.len() / (4 * channel_data_tx.len()));
+            channel_data_tx.len()
+        ];
+        for (chan, data) in (0..channel_data_tx.len()).cycle().zip(
+            data.chunks(4)
+                .map(|x| f32::from_le_bytes((&x[..4]).try_into().unwrap())),
+        ) {
+            channels[chan].push(data);
+        }
+        for (data, sink) in channels.drain(..).zip(&channel_data_tx) {
+            sink.send(data.into()).await?;
         }
     }
     Ok(())
