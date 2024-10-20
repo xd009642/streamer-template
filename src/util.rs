9da964ca23a535ab1b704eae5ce25deb366b784c
xd009642 <danielmckenna93@gmail.com>
2024-10-20 15:58:01 +0100

    Get a bit further on instrumenting spawn

--- src/util.rs
+++ src/util.rs
@@ -1,19 +1,37 @@
-use tokio::task;
 use std::future::Future;
+use tokio::task;
 
+// Here we need to pass in some sort of handle to our panic counting metric
+// which is shareable. Maybe just an `Arc<AtomicUsize>` or whatever the measured counter type is.
 
-pub fn spawn<F>(future: F) -> JoinHandle<F::Output> ⓘ
+pub fn spawn<F>(future: F) -> impl Future<Output = F::Output>
 where
     F: Future + Send + 'static,
     F::Output: Send + 'static,
 {
-    todo!()
+    async move {
+        let res = task::spawn(future).await;
+        match res {
+            Ok(v) => v,
+            Err(e) => {
+                todo!("Do some metrics stuff")
+            }
+        }
+    }
 }
 
-pub fn spawn_blocking<F, R>(f: F) -> JoinHandle<R> ⓘ
+pub fn spawn_blocking<F, R>(f: F) -> impl Future<Output = R>
 where
     F: FnOnce() -> R + Send + 'static,
     R: Send + 'static,
 {
-    todo!()
+    async move {
+        let res = task::spawn_blocking(f).await;
+        match res {
+            Ok(v) => v,
+            Err(e) => {
+                todo!("Do some metrics stuff")
+            }
+        }
+    }
 }
