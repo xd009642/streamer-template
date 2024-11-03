9209aa0cf0cdf400fa012c171cf4efd124af99b6
xd009642 <danielmckenna93@gmail.com>
2024-11-03 12:24:14 +0000

    set buckets add RTF metrics to model

--- src/metrics.rs
+++ src/metrics.rs
@@ -5,8 +5,8 @@
 //! 1. Works for google and we're lower scale
 //! 2. leaves choice up to metrics consumers on how to grab things
 //! 3. More dynamic
-use metrics::{counter, describe_counter, histogram, Counter, Histogram, Unit};
-use metrics_exporter_prometheus::{PrometheusBuilder, PrometheusHandle};
+use metrics::{counter, describe_counter, describe_histogram, histogram, Counter, Histogram, Unit};
+use metrics_exporter_prometheus::{Matcher, PrometheusBuilder, PrometheusHandle};
 use quanta::{Clock, Instant};
 use std::time::Duration;
 use tokio_metrics::{TaskMetrics, TaskMonitor};
@@ -21,6 +21,7 @@ impl AppMetricsEncoder {
         let builder = PrometheusBuilder::new();
 
         describe_task_metrics();
+        let builder = describe_audio_metrics(builder);
 
         let prometheus_handle = builder.install_recorder().unwrap();
         Self {
@@ -44,6 +45,29 @@ pub struct StreamingMonitors {
     pub client_receiver: TaskMonitor,
     pub audio_decoding: TaskMonitor,
     pub inference: TaskMonitor,
+}
+
+fn describe_audio_metrics(builder: PrometheusBuilder) -> PrometheusBuilder {
+    describe_histogram!(
+        "media_duration_seconds",
+        Unit::Seconds,
+        "Duration of audio."
+    );
+    describe_histogram!(
+        "processing_duration_seconds",
+        Unit::Seconds,
+        "Duration of processing time."
+    );
+    describe_histogram!("rtf", "Real-Time Factor of the processing");
+
+    builder
+        .set_buckets_for_metric(
+            Matcher::Suffix("seconds".to_string()),
+            RtfMetric::duration_buckets(),
+        )
+        .unwrap()
+        .set_buckets_for_metric(Matcher::Suffix("rtf".to_string()), RtfMetric::rtf_buckets())
+        .unwrap()
 }
 
 fn describe_task_metrics() {
@@ -178,16 +202,22 @@ impl RtfMetric {
     /// which feel right. It may take some tuning and adapting as things go and may change based on
     /// models or things like what type of GPU you use. I'll just pick some pretty arbitrary
     /// measurements that feel ok.
-    const fn rtf_buckets(&self) -> &[f64] {
-        match self {
-            Self::Audio => &[
-                0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5,
-            ],
-            Self::Model => &[
-                0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0, 1.25, 1.5, 1.75, 2.0, 4.0, 6.0, 8.0, 10.0, 15.0,
-            ],
-            Self::Vad => &[0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0],
-        }
+    ///
+    /// This is also a moderately large histogram that covers all our stages despite some of them
+    /// being a lot bigger. This is because I'm not too sure about specifying different buckets
+    /// based on the metric fields (not just name).
+    const fn rtf_buckets() -> &'static [f64] {
+        &[
+            0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.75, 1.0, 1.1, 1.2, 1.3, 1.4, 1.5, 1.75, 2.0, 4.0, 6.0,
+            8.0, 10.0, 15.0,
+        ]
+    }
+
+    const fn duration_buckets() -> &'static [f64] {
+        &[
+            0.25, 0.5, 0.75, 1.0, 2.0, 3.0, 4.0, 5.0, 7.5, 10.0, 15.0, 20.0, 30.0, 60.0, 90.0,
+            120.0, 300.0,
+        ]
     }
 }
 
@@ -203,8 +233,10 @@ impl RtfMetricGuard {
     pub fn new(audio_duration: Duration, metric: RtfMetric) -> Self {
         let clock = Clock::new();
         let name = metric.name();
-        histogram!("media_duration", "pipeline" => name).record(audio_duration.as_secs_f64());
-        let processing_duration = histogram!("processing_duration", "pipeline" => name);
+        histogram!("media_duration_seconds", "pipeline" => name)
+            .record(audio_duration.as_secs_f64());
+        let processing_duration =
+            histogram!("processing_duration_seconds", "pipeline_stage" => name);
         let rtf = histogram!("rtf", "pipeline" => name);
         let now = clock.now();
         Self {
