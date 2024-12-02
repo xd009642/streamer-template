9f28176b5304d2d9ef4b0b5ff303510df8339a76
xd009642 <danielmckenna93@gmail.com>
2024-12-02 13:06:54 +0000

    Spawn the main loop

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,16 @@
 #![cfg(not(tarpaulin_include))]
+#![allow(clippy::disallowed_methods)]
+use tokio::task;
 
 #[tokio::main]
 async fn main() {
     init_tracing_opentelemetry::tracing_subscriber_ext::init_subscribers()
         .expect("Failed to setup otel");
-    // So for launching either axum or actix we don't want to use the macros to make our tokio
-    // runtime because actix takes a different approach (multiple single threaded runtimes).
-    streamer_template::launch_server().await;
+
+    // So using our disallowed method here just to launch the server. The main worker thread can't
+    // run spawned tasks so it causes a forced context switch. So we want to make sure things
+    // aren't ran on the main worker.
+    task::spawn(streamer_template::launch_server())
+        .await
+        .unwrap();
 }
